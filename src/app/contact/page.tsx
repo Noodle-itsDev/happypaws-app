998f936b5db2cd9edfca5085648c7f2718c114fb
KJoTechTalent <acortec.johana@gmail.com>
2024-07-29 01:00:46 +0200

    Login
    Se realizan modificaciones

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react";
+
+const ContactPage: React.FC = () => {
+
+    return(
+        <h1>
+            Contact Page
+        </h1>
+    );
+
+}
+
+export default ContactPage;
